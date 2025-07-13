131d40abd7e64c4b8a40752e6212e70c03fef44f
Karanbir Singh <karan0031singh@gmail.com>
2025-07-13 21:26:52 +0530

    set pager snap helper for recycler view full scrooling

--- app/src/main/java/com/example/playstoreuilayout/MainActivity.kt
+++ app/src/main/java/com/example/playstoreuilayout/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.example.playstoreuilayout
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import com.example.playstoreuilayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val  snapHelperHead= PagerSnapHelper()
+    private val  snapHelperBody= PagerSnapHelper()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.apply {
+            snapHelperHead.attachToRecyclerView(rcvHeader)
+            snapHelperBody.attachToRecyclerView(rcvBodyApp)
+
+            rcvHeader.adapter = RcvHeaderAdapter()
+
+            rcvBodyApp.layoutManager =
+                GridLayoutManager(
+                    this@MainActivity,
+                    3, GridLayoutManager.HORIZONTAL,
+                    false
+                )
+            rcvBodyApp.adapter = RcvBodyAppsGridAdapter()
+
+        }
+
 
     }
 }
