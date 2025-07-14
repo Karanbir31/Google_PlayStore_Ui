be5584498efc96a11dd9371f6dd5ae9beaa62e62
Karanbir Singh <karan0031singh@gmail.com>
2025-07-14 11:28:54 +0530

    bottom nav, tabs layout with viewpager, all recycler views with nested rcv is working now

--- app/src/main/java/com/example/playstoreuilayout/TopChartsFragment.kt
+++ app/src/main/java/com/example/playstoreuilayout/TopChartsFragment.kt
@@ -1,12 +1,23 @@
 package com.example.playstoreuilayout
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.playstoreuilayout.databinding.FragmentTopChartsBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class TopChartsFragment : Fragment() {
+
+    private lateinit var binding: FragmentTopChartsBinding
+    private val tabsList = listOf(
+        "For you",
+        "Top charts",
+        "Children",
+        "Categories"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +27,28 @@ class TopChartsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_top_charts, container, false)
+        binding = FragmentTopChartsBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+
+            viewpage2.adapter = ViewPager2Adapter(tabsList, childFragmentManager, lifecycle)
+
+            TabLayoutMediator(tabLayout, viewpage2) { tab, position ->
+                tab.text = tabsList[position]
+                Log.d("tab text", "${tab.text}")
+
+            }.attach()
+        }
+
+
     }
 }
--- app/src/main/java/com/example/playstoreuilayout/ViewPager2Adapter.kt
+++ app/src/main/java/com/example/playstoreuilayout/ViewPager2Adapter.kt
@@ -1,4 +1,26 @@
 package com.example.playstoreuilayout
 
-class ViewPager2Adapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+
+class ViewPager2Adapter(
+    private val tabsList: List<String>,
+    private val fragmentManager: FragmentManager,
+    private val lifecycle: Lifecycle
+) : FragmentStateAdapter(
+    fragmentManager, lifecycle
+) {
+    override fun getItemCount(): Int = tabsList.size
+
+    override fun createFragment(position: Int): Fragment {
+        when(position){
+            1-> return HomeFragment()
+            2-> return CategoriesFragment()
+            else -> CategoriesFragment()
+        }
+        return HomeFragment()
+    }
 }
